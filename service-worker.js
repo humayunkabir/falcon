436e278fc674e15276be98dade30a110598ff54e
Devs Valley <devsvalley.xvi@gmail.com>
2019-12-26 02:17:14 +0600

    live fixed

--- service-worker.js
+++ service-worker.js
@@ -13,7 +13,7 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
-importScripts('/precache-manifest.02e1c8a1854df5bef85da62f6c43ded2.js');
+importScripts('/precache-manifest.676a170ba8976fdba2cfe1ba5297a63f.js');
 
 self.addEventListener('message', event => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
